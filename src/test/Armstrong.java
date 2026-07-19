4d99cae80ddc496ec343dd08c135b65bee8fda4c
Hp <Hp@192.168.1.44>
2026-07-19 14:33:12 +0530

    java

---
+++ src/test/Armstrong.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int digits = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+        int sum = 0;
+
+        while (temp != 0) {
+            int rem = temp % 10;
+            sum += Math.pow(rem, digits);
+            temp /= 10;
+        }
+
+        if (sum == num)
+            System.out.println(num + " is an Armstrong Number.");
+        else
+            System.out.println(num + " is Not an Armstrong Number.");
+
+        sc.close();
+    }
+}
